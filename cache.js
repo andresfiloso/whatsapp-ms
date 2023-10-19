cc0c6cb6458a9fb0b7c35a59512d9fd31ba9e993
Andres Filoso <andresfilosok@gmail.com>
2023-10-18 23:20:34 -0300

    update library

--- cache.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("async-redis");
-const config = {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-};
-const cache = createClient(config);
-cache.auth(process.env.REDIS_PASSWORD);
-
-module.exports = cache;
--- routes/chat.js
+++ routes/chat.js
@@ -1,26 +1,21 @@
-const router = require("express").Router();
-const {MessageType } = require("@adiwajshing/baileys");
+const router = require('express').Router();
 
-router.post("/sendmessage/:phone", async (req, res) => {
+router.post('/sendmessage/:phone', async (req, res) => {
   let phone = req.params.phone;
   let message = req.body.message;
 
   if (phone == undefined || message == undefined) {
     res.send({
-      status: "error",
+      status: 'error',
       message: `Please enter valid phone and message`,
     });
   } else {
-    await client
-      .sendMessage(phone + "@c.us", message, MessageType.text)
-      .then((response) => {
-        if (response.key.fromMe) {
-          res.send({
-            status: "success",
-            message: `MessageType.text successfully sent to ${phone}`,
-          });
-        }
-      });
+    const result = await client.sendMessage(`${phone}@c.us`, message);
+    console.log('result', result);
+    res.send({
+      status: 'success',
+      message: `MessageType.text successfully sent to ${phone}`,
+    });
   }
 });
 
